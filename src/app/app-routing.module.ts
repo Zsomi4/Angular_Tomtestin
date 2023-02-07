388f12b8c6e3b94c331dd22b83ecc03f08e71a92
Zsomi4 <k72302872172@ktch.hu>
2023-02-07 10:39:42 +0100

    Komponensek létrehozása és elérési út megadása a fejléc elemeire.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: "about", component: AboutComponent},
+  {path: "github", component: GithubComponent},
+  {path: "", component: HomeComponent}
 ];
 
 @NgModule({
