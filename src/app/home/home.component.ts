a7648f3b2af0283d1b7a64c301283eaab46d8af0
Zsomi4 <k72302872172@ktch.hu>
2023-02-07 10:49:59 +0100

    calculateWeightAre megirása és ReactiveForm importálása.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  weightAreaForm !: FormGroup;
+  weightArea !: number;
+
+  ngOnInit(): void {
+    this.weightAreaForm = new FormGroup({
+      weight: new FormControl(0),
+      height: new FormControl(0)
+    });
+  }
+
+  calculateWeightArea() {
+    const {weight, height} = this.weightAreaForm.value;
+    this.weightArea = weight / Math.pow(height, 2);
+  }
 
 }
